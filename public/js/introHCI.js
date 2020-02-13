ce48fcde8c7ff6dbc31a88b77778cdc4ce178cc6
thomastangerine <thomastang.thomas97@gmail.com>
2020-02-13 14:37:21 -0800

    Finish lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,29 +2,39 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+    $('.project a').click(addProjectDetails);
 
-	// $('#colorBtn').click(randomizeColors);
+    // $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+    // Prevent following the link
+    e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+    // Get the div ID, e.g., "project3"
+    var projectID = $(this).closest('.project').attr('id');
+    // get rid of 'project' from the front of the id 'project3'
+    var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+    var projectURL = $.get("http://localhost:3000/project/" + idNumber, callBackFunc);
+
+    console.log("User clicked on project " + idNumber);
+    console.log("Project URL " + "http://localhost:3000/project/" + idNumber);
+}
+
+function callBackFunc(result) {
+    $("#project" + result.id + " .details").append("<p>" + result.title + "</p");
+    $("#project" + result.id + " .details").append("<p>" + result.date + "</p");
+    $("#project" + result.id + " .details").append("<img class=\"detailsImage\", src=\"" + result.image + "\"></img>");
+    $("#project" + result.id + " .details").append(result.summary);
 }
